2f3b48cff407dce9b1fa204d5c8106193765823b
Rich Lysogorski <rmlysogorski@gmail.com>
2019-06-14 14:27:58 -0400

    Fixed Model

--- UnSpaceWebApp/Models/IdentityModels.cs
+++ UnSpaceWebApp/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace UnSpaceWebApp.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("UnSpaceDb", throwIfV1Schema: false)
         {
         }
 
