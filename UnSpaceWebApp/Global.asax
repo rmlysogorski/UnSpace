10b536720a8ed040145f3a9f8f4e622f723e31f6
Rich Lysogorski <rmlysogorski@gmail.com>
2019-06-14 14:13:56 -0400

    First commit

---
+++ UnSpaceWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UnSpaceWebApp.MvcApplication" Language="C#" %>
---
+++ UnSpaceWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnSpaceWebApp.Startup))]
+namespace UnSpaceWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
