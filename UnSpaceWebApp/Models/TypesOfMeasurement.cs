e8d9fe87ef5e2d00d8fc92f7d7e69ca38aa9bf47
Rich Lysogorski <rmlysogorski@gmail.com>
2019-06-17 15:24:18 -0400

    Wayfair API stuff

---
+++ UnSpaceWebApp/Models/TypesOfMeasurement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnSpaceWebApp.Models
+{
+    public enum TypesOfMeasurement
+    {
+        ft,
+        @in,
+        cm       
+
+    }
+}
