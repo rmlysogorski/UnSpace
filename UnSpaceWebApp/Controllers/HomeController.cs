0c8297afe36a41562732db4a95872fe4180aacc7
Andre Watts <andrewattsii@gmail.com>
2019-06-17 15:08:21 -0400

    andres

--- UnSpaceWebApp/Controllers/HomeController.cs
+++ UnSpaceWebApp/Controllers/HomeController.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace UnSpaceWebApp.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult ListSpaces()
+        {
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
