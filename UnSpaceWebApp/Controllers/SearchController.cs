301403cab48f153ed96a7cf0a6543f70481f15f8
Rich Lysogorski <rmlysogorski@gmail.com>
2019-06-18 08:40:23 -0400

    Search by keyword added

---
+++ UnSpaceWebApp/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnSpaceWebApp.Models;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+
+namespace UnSpaceWebApp.Models
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList(string searchQ)
+        {
+            JObject data = EtsyDAL.GetEtsyAPI("&category=furniture&keywords=" + searchQ);
+            List<EtsyItem> items = new List<EtsyItem>();
+            for (int i = 0; i < 25; i++)
+            {
+                EtsyItem newItem = new EtsyItem();
+                newItem.Listing_Id = data["results"][i]["listing_id"].ToString();
+                items.Add(newItem);
+            }
+            return View("Index",items);
+        }
+
+
+    }
+}
