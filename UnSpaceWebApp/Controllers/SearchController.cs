01118db97aaabc91ab43e72f929fa23cce83e43d
Rich Lysogorski <rmlysogorski@gmail.com>
2019-06-19 11:12:04 -0400

    Sewed up Space page

--- UnSpaceWebApp/Controllers/SearchController.cs
+++ UnSpaceWebApp/Controllers/SearchController.cs
@@ -14,126 +14,78 @@ namespace UnSpaceWebApp.Models
     public class SearchController : Controller
     {
         // GET: Search
-        private static int countPage;
-        private List<EtsyItem> items = new List<EtsyItem>();
         public ActionResult Index()
         {
             return View();
+        }     
+        
+        public ActionResult FurnitureList()
+        {
+            return PartialView();
         }
 
-        public ActionResult NewProductList(string searchQ)
+        [HttpPost]
+        public ActionResult FurnitureList(string SearchQ, string pageNo = "")
         {
-            if(Session["data"] != null)
-            {
-                Session.Remove("data");
-            }
-            countPage = 0;
-            return RedirectToAction("ProductList", "Search", new { searchQ });
-        }
-
-        public ActionResult ProductList(string searchQ, string forOrBack = "")
-        {
-            ViewBag.SearchQ = searchQ;
-            JObject data = new JObject();
-            if(Session["data"] == null)
-            {
-                data = EtsyDAL.GetEtsyAPI("&category=furniture&keywords=" + searchQ, "active");
-                Session["data"] = data;
-            }
-            else
-            {
-                data = (JObject)Session["data"];
-            }                        
-
-            int min = 0;
-            int max = 5;
-            if(forOrBack == "for")
-            {
-                countPage++;
-            }
-            else if (forOrBack == "back")
-            {
-                countPage--;
-            }
-
-            int pagePrev;
-            int pageNext;
-
-            if (countPage == 0)
-            {
-                pagePrev = (int)data["pagination"]["effective_page"] - 1;
-                if (pagePrev > 0)
+            List<EtsyItem> items = new List<EtsyItem>();
+            if (SearchQ != null)
+            {   
+                if(pageNo != string.Empty)
                 {
-                    ViewBag.Prev = pagePrev.ToString();
+                    if (int.Parse(pageNo) > 1)
+                    {
+                        SearchQ += "&page=" + pageNo;
+                    }
+                }
+                JObject data = EtsyDAL.GetEtsyAPI("&limit=5&category=furniture&keywords=" + SearchQ, "active");
+                if (SearchQ.Contains('&'))
+                {
+                    TempData["SearchQ"] = SearchQ.Split('&')[0];
+                }
+                else
+                {
+                    TempData["SearchQ"] = SearchQ;
+                }
+                int prevPage = (int)data["pagination"]["effective_page"];
+                if (prevPage > 1)
+                {
+                    TempData["prevPage"] = prevPage - 1;
+                }
+                else
+                {
+                    TempData["prevPage"] = null;
+                }
+                TempData["nextPage"] = (int)data["pagination"]["next_page"];
+                for (int i = 0; i < data["results"].Count(); i++)
+                {
+                    EtsyItem newItem = new EtsyItem();
+                    newItem.Listing_Id = data["results"][i]["listing_id"].ToString();
+                    newItem.Title = data["results"][i]["title"].ToString();
+                    newItem.Url = data["results"][i]["url"].ToString();
+                    newItem.Price = data["results"][i]["price"].ToString();
+                    newItem.Item_Width = data["results"][i]["item_width"].ToString();
+                    newItem.Item_Length = data["results"][i]["item_length"].ToString();
+                    newItem.Item_Height = data["results"][i]["item_height"].ToString();
+                    newItem.Item_Dimensions_unit = data["results"][i]["item_dimensions_unit"].ToString();
+                    newItem.Currency_Code = data["results"][i]["currency_code"].ToString();
+                    JObject imageData = EtsyDAL.GetEtsyAPI(newItem.Listing_Id, "image");
+                    newItem.ImageThumbUrl = imageData["results"][0]["url_75x75"].ToString();
+                    newItem.ImageFullUrl = imageData["results"][0]["url_fullxfull"].ToString();
+                    items.Add(newItem);
                 }
             }
-
-            if (countPage == 4)
-            {
-                pageNext = (int)data["pagination"]["next_page"];
-                ViewBag.Next = pageNext.ToString();
-            }
-
-            switch (countPage)
-            {
-                case 0:
-                    min = 0;
-                    max = 5;
-                    break;
-                case 1:
-                    min = 5;
-                    max = 10;
-                    break;
-                case 2:
-                    min = 10;
-                    max = 15;
-                    break;
-                case 3:
-                    min = 15;
-                    max = 20;
-                    break;
-                case 4:
-                    min = 20;
-                    max = 25;
-                    break;
-            }
-
-            ViewBag.CountPage = countPage;
-            for (int i = min; i < max; i++)
-            {
-                EtsyItem newItem = new EtsyItem();
-                newItem.Listing_Id = data["results"][i]["listing_id"].ToString();
-                newItem.Title = data["results"][i]["title"].ToString();
-                newItem.Url = data["results"][i]["url"].ToString();
-                newItem.Price = data["results"][i]["price"].ToString();
-                newItem.Item_Width = data["results"][i]["item_width"].ToString();
-                newItem.Item_Length = data["results"][i]["item_length"].ToString();
-                newItem.Item_Height = data["results"][i]["item_height"].ToString();
-                newItem.Item_Dimensions_unit = data["results"][i]["item_dimensions_unit"].ToString();
-                newItem.Currency_Code = data["results"][i]["currency_code"].ToString();
-                JObject imageData = EtsyDAL.GetEtsyAPI(newItem.Listing_Id,"image");
-                newItem.ImageThumbUrl = imageData["results"][0]["url_75x75"].ToString();
-                newItem.ImageFullUrl = imageData["results"][0]["url_fullxfull"].ToString();
-                items.Add(newItem);
-            }
-            return View("Index",items);
+            TempData["items"] = items;
+            return RedirectToAction("Index", "Space");
         }
 
-        public ActionResult SearchPage(string pageNo, string searchQ, string forOrBack)
+        public ActionResult SearchPage(int pageNo, string searchQ)
         {
-            string newSearchQ = "&category=furniture&keywords=" + searchQ + "&page=" + pageNo;
-            Session["data"] = EtsyDAL.GetEtsyAPI(newSearchQ, "active"); ;
-
-            if (forOrBack == "for")
+            string newSearchQ = searchQ;
+            if (pageNo > 1)
             {
-                countPage = 0;
+                newSearchQ += "&page=" + pageNo.ToString();
             }
-            
-            if(forOrBack == "back")
-            {
-                countPage = 4;
-            }
-            return RedirectToAction("ProductList", "Search", new { searchQ = newSearchQ });
+            return RedirectToAction("FurnitureList", "Search", new { SearchQ = newSearchQ });
         }
     }
 }
--- UnSpaceWebApp/Controllers/SpaceController.cs
+++ UnSpaceWebApp/Controllers/SpaceController.cs
@@ -12,10 +12,35 @@ namespace UnSpaceWebApp.Controllers
 {
     public class SpaceController : Controller
     {
+        public static MySpace thisSpace = new MySpace();
         // GET: Space
         public ActionResult Index()
         {
-            return View();
+            if(TempData["items"] != null)
+            {
+                thisSpace.items = (List<EtsyItem>)TempData["items"];
+            }
+            if(TempData["prevPage"] != null)
+            {
+                ViewBag.PrevPage = TempData["prevPage"];
+            }
+            if(TempData["nextPage"] != null)
+            {
+                ViewBag.NextPage = TempData["nextPage"];
+            }
+            if(TempData["SearchQ"] != null)
+            {
+                ViewBag.SearchQ = TempData["SearchQ"];
+            }
+            return View(thisSpace);
         }  
+
+        public ActionResult GenerateSpace(string Width, string Length, string Measurement)
+        {
+            thisSpace.SpaceDimensions.Width = Width;
+            thisSpace.SpaceDimensions.Length = Length;
+            thisSpace.SpaceDimensions.Measurement = Measurement;
+            return View("Index", thisSpace);
+        }
     }
 }
