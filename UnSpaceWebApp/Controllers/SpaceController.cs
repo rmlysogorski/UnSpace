ec2a125a95e8b873763d43c105d0cfff89e87f0f
Chris Phillips <you@example.com>
2019-06-19 11:14:17 -0400

    Listing the Spaces

--- UnSpaceWebApp/Controllers/SpaceController.cs
+++ UnSpaceWebApp/Controllers/SpaceController.cs
@@ -12,10 +12,24 @@ namespace UnSpaceWebApp.Controllers
 {
     public class SpaceController : Controller
     {
+        UserSpacesEntities ORM = new UserSpacesEntities();
         // GET: Space
         public ActionResult Index()
         {
             return View();
         }  
+
+        public static string AddToList(List<string> Listing)
+        {
+
+            string listing = string.Empty;
+            for(int i=0;i<Listing.Count; i++)
+            {
+                listing += Listing[i] + ",";
+
+            }
+            return listing;
+        }
+
     }
 }
