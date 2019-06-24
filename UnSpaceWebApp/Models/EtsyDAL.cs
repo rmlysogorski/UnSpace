53e82e81acb2cfe00d6080aa654e9c46c387830f
Andre Watts <andrewattsii@gmail.com>
2019-06-24 10:50:14 -0400

    2 changes spacecontroller and etsy etsydal

--- UnSpaceWebApp/Models/EtsyDAL.cs
+++ UnSpaceWebApp/Models/EtsyDAL.cs
@@ -32,7 +32,7 @@ namespace UnSpaceWebApp.Models
             HttpWebRequest request = WebRequest.CreateHttp(url);
             request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();fg
 
             if (response.StatusCode == HttpStatusCode.OK)
             {
